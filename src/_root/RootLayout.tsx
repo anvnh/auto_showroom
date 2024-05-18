807501a871f52d3ebb631b2a46f1fb1d1147c154
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-05-19 02:22:53 +0700

    animate silde

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,35 +1,48 @@
 
 import {Navbar, Hero, Product, Product2 ,Banner, Footer} from './_homepage'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect, useRef } from "react"; // Import both useEffect and useRef
+
 const RootLayout = () => {
+    useEffect(() => {
+		AOS.init({
+			duration: 2000,
+			easing: "ease-in-out",
+			once: false,
+			mirror: true,
+			anchorPlacement: "top-bottom",
+		});
+	}, []);
     return (
         <section className="w-full">
             <div className="w-full overflow-hidden bg-primary">
-                <div className="flex items-start justify-center">
+                <div data-aos="zoom-out" className="flex items-start justify-center ">
                     <div className="w-full">
                         <Navbar />
                     </div>
                 </div>
-                <div className="flex items-start justify-center bg-primary">
+                <div data-aos="zoom-out" className="flex items-start justify-center bg-primary">
                     <div className="w-full">
                         <Hero />
                     </div>
                 </div>
-                <div className="flex items-start justify-center  bg-primary ">
+                <div data-aos="zoom-out" className="flex items-start justify-center  bg-primary ">
                     <div className="w-full">
                         <Product />
                     </div>
                 </div>
-                <div className="bg-primary flex items-start pt-12">
+                <div data-aos="zoom-out" className="bg-primary flex items-start pt-12">
                     <div className="w-full">
                         <Product2 />
                     </div>
                 </div>         
-                <div className="flex items-start justify-center bg-primary ">
+                <div data-aos="zoom-out" className="flex items-start justify-center bg-primary ">
                     <div className="w-full">
                         <Banner />
                     </div>
                 </div>   
-                <div className="flex items-start justify-center bg-primary">
+                <div data-aos="zoom-out" className="flex items-start justify-center bg-primary">
                     <div className="w-full">
                         <Footer />
                     </div>
