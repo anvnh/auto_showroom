47f17e6dd71c4b3169b13367099e77379b087047
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-07-03 23:54:20 +0700

    repon 2%

--- client/src/_root/_homepage/Navbar.tsx
+++ client/src/_root/_homepage/Navbar.tsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import {
-  logo,
-} from "../../assets";
+import { logo, menu, close } from "../../assets";
 
 import Vehicle from '../elementNavbar/Vehicle';
 import ShoppingAssist from '../elementNavbar/ShoppingAssist';
+
 interface SubNavbarProps {
   selectedSection_element: string;
   onNavClick: (section: string) => void;
@@ -126,7 +125,7 @@ const Navbar: React.FC<SubNavbarProps> = ({
       {isVisible && (
         <nav
           data-aos="fade"
-          className="w-full pt-3 h-16 flex pb-2 bg-gray-950 md:px-12.5 z-50 relative"
+          className="w-full pt-3 h-14 md:h-16 flex pb-2 bg-gray-950 md:px-12.5 z-50 relative"
         >
           <div className="absolute left-1/2 transform -translate-x-1/2">
             <Link to="/">
@@ -134,28 +133,41 @@ const Navbar: React.FC<SubNavbarProps> = ({
                 data-aos="fade-up"
                 src={logo}
                 alt="logo"
-                className="md:w-[57px] w-[55px] md:h-[50px] h-[55px]"
+                className="md:w-[57px] w-[35px] md:h-[50px] h-[35px]"
               />
             </Link>
           </div>
-          <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-            <Link to="/SignIn">
-              <li className="relative group pr-5">
-                <FaUser className="text-white w-[27px] h-[27px] transform hover:scale-110 transition-transform duration-300 cursor-pointer" />
-              </li>
-            </Link>
-          </ul>
+          <div className="flex-1 flex justify-end items-center">
+            <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+              <Link to="/SignIn">
+                <li className="relative group pr-5">
+                  <FaUser className="text-white w-[27px] h-[27px] transform hover:scale-110 transition-transform duration-300 cursor-pointer" />
+                </li>
+              </Link>
+            </ul>
+            <button
+              className="sm:hidden text-white pr-3 focus:outline-none"
+              onClick={() => setToggle(!toggle)}
+            >
+              <img
+					src={toggle ? close : menu}
+					alt="menu"
+					className="w-[24px] h-[24px] object-contain"
+					onClick={() => setToggle(!toggle)}
+				/>
+            </button>
+          </div>
         </nav>
       )}
       {isVisible && (
         <div
-          className={`z-50 top-0 w-full font-poppins transition-transform duration-300 ${
+          className={`z-50 md:top-12 w-full font-poppins transition-transform duration-300 sm:block ${
             isHidden ? "-translate-y-full" : "translate-y-0"
           }`}
         >
           <nav
-            data-aos="fade-left"
-            className="w-full pt-3 h-16 flex opacity-10 pb-2 text-white md:px-12.5 z-50 relative backdrop-filter backdrop-blur-3xl"
+            data-aos="fade-up"
+            className="w-full pt-4 h-14 opacity-10 text-white md:px-12.5 z-50 relative backdrop-blur-3xl bg-gray-950 bg-opacity-50 sm:block hidden"
           >
             <ul className="list-none sm:flex gap-10 justify-center items-center flex-1">
               <li
@@ -212,23 +224,84 @@ const Navbar: React.FC<SubNavbarProps> = ({
 
       {isExpanded && selectedSection === "vehicles" && (
         <div
-          data-aos="slide-up"
-          className={`z-1 absolute w-screen h-[450px] ss:h-[900px] font-syncopate bg-opacity-85 bg-gray-900`}
-        >
+        data-aos="slide-up"
+        className={`z-1 absolute w-screen h-[450px] ss:h-[900px] font-syncopate bg-opacity-85 bg-gray-900 ${
+          isHidden ? 'hidden' : ''
+        }`}
+      >
          
-          <div  data-aos="fade-up" data-aos-delay="1000">
+          <div  data-aos="fade-up" data-aos-delay="1000"   className={`${
+          isHidden ? 'hidden' : ''
+        }`}
+      >
              <Vehicle />
             </div>
         </div>
       )}
       {isExpanded && selectedSection === "shopping" && (
         <div
-          data-aos="slide-up"
-          className={`z-1 absolute w-screen h-[450px] ss:h-[900px] rounded-b-[20px] font-syncopate px-[10px] lg:px-[50px] pt-[28px] bg-opacity-85 bg-gray-900`}
-        >
-             <div>
+        data-aos="slide-up"
+        className={`z-1 absolute w-screen h-[450px] ss:h-[900px] rounded-b-[20px] font-syncopate px-[10px] lg:px-[50px] pt-[28px] bg-opacity-85 bg-gray-900 ${isHidden ? 'hidden' : ''}`}
+      >
+            <div  data-aos="fade-up" data-aos-delay="1000"   className={`${
+          isHidden ? 'hidden' : ''
+        }`}
+      >
              <ShoppingAssist />
             </div>
+        </div>
+      )}
+
+      {toggle && (
+        <div data-aos="fade-up" className="sm:hidden opacity-90 h-[620px] relative w-full backdrop-blur-3xl bg-gray-950 bg-opacity-50 text-white">
+          <ul className="flex flex-col space-y-5 pt-52 items-center py-2">
+
+            <li
+              className={`cursor-pointer transition-opacity duration-300 w-full text-center ${
+                selectedP && selectedP !== "vehicles"
+                  ? "opacity-50"
+                  : "opacity-100"
+              }`}
+              onMouseEnter={() => handleMouseEnter("vehicles")}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleToggleSection("vehicles")}
+            >
+              <div data-aos="fade-up" 	data-aos-delay="1000" className="relative group flex justify-center transition ease-in-out delay-100 duration-300 select-none">
+                <p> Vehicles </p>
+       
+              </div>
+            </li>
+            <li
+              className={`cursor-pointer transition-opacity duration-300 w-full text-center ${
+                selectedP && selectedP !== "shopping"
+                  ? "opacity-50"
+                  : "opacity-100"
+              }`}
+              onMouseEnter={() => handleMouseEnter("shopping")}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleToggleSection("shopping")}
+            >
+              <div data-aos="fade-up" 	data-aos-delay="1200" className="relative group flex justify-center transition ease-in-out delay-100 duration-300 select-none">
+                <p> Shopping Assist </p>   
+                </div>    
+            </li>
+            <li
+              className={`cursor-pointer transition-opacity duration-300 w-full text-center ${
+                selectedP && selectedP !== "owners"
+                  ? "opacity-50"
+                  : "opacity-100"
+              }`}
+              onMouseEnter={() => handleMouseEnter("owners")}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleToggleSection("owners")}
+            >
+              <div data-aos="fade-up" 	data-aos-delay="1400" className="relative group flex justify-center transition ease-in-out delay-100 duration-300 select-none">
+                <Link to="/owners">
+                  <p> Owners </p>             
+                </Link>
+              </div>
+            </li>
+          </ul>
         </div>
       )}
     </div>
