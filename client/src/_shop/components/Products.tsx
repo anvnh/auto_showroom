af69a43efcb128005e16e6f7c9ef67ba7a6484c7
anvnh <anvo20052@gmail.com>
2024-07-05 13:07:04 +0700

    Update pagination

--- client/src/_shop/components/Products.tsx
+++ client/src/_shop/components/Products.tsx
@@ -5,16 +5,29 @@ import { FaCogs } from 'react-icons/fa';
 import { Link } from "react-router-dom";
 import { MdArrowOutward } from "react-icons/md";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+
+
 
 const Products = () => {
+	const [currentPage, setCurrentPage] = useState(1);
+	const productsPerPage = 4;
 
 	// get all products
-	const { data: products, isLoading, refetch, isRefetching } = useQuery({
+	const {
+		data: products,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
 		queryKey: ["products"],
 		queryFn: async () => {
 			try {
 				const response = await fetch("/api/car/all");
 				const data = await response.json();
+
+				console.log(data);
 
 				if (!response.ok) {
 					throw new Error(data.message || "Something went wrong!");
@@ -27,59 +40,92 @@ const Products = () => {
 		},
 	});
 
+	// calculate 
+	const indexOfLastProduct = currentPage * productsPerPage;
+	const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+	const currentProducts = products
+		? products.slice(indexOfFirstProduct, indexOfLastProduct)
+		: [];
+	const totalPages = products
+		? Math.ceil(products.length / productsPerPage)
+		: 0;
+
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
 	return (
-		<div className="flex flex-warp gap-4 w-full px-36">
-			{/* Map through products */}
-			{!isLoading && !isRefetching && products?.length === 0 && (
-				<p className="text-center my-4">No products available</p>
-			)}
-			{!isLoading &&
-				!isRefetching &&
-				products &&
-				products.map((product) => (
-					<div className="w-[500px] rounded-3xl overflow-hidden shadow-lg bg-white hover:bg-opacity-85">
-						<div className="relative">
-							<img
-								className="w-full h-48 object-cover"
-								src={product.image}
-								alt="Car"
-							/>
-							<button className="absolute top-2 right-2 bg-white p-2 rounded-full shadow-md">
-								<CiBookmark className="text-xl text-gray-700" />
-							</button>
-						</div>
-						<div className="p-4 text-black">
-							<h2 className="text-xl font-bold">
-								{product.brand} {product.car_model}
-							</h2>
-							<p className="text-sm">{product.bio}</p>
-							<div className="flex items-center justify-between mt-4 text-gray-700 mx-3">
-								<div className="items-center mr-4">
-									<RiSpeedUpFill className="w-5 h-5 mr-1" />
-									{product.performance.top_speed}
+		<>
+			<div className="grid grid-cols-4 gap-4 w-full px-36">
+				{/* Map through products */}
+				{!isLoading && !isRefetching && products?.length === 0 && (
+					<p className="text-center my-4">No products available</p>
+				)}
+				{!isLoading &&
+					!isRefetching &&
+					currentProducts &&
+					currentProducts.map((product) => (
+						<div className="w-[360px] rounded-3xl overflow-hidden shadow-lg bg-white hover:bg-opacity-85">
+							<div className="relative">
+								<img
+									className="w-full h-48 object-cover"
+									src={product.image}
+									alt="Car"
+								/>
+								<button className="absolute top-2 right-2 bg-white p-2 rounded-full shadow-md">
+									<CiBookmark className="text-xl text-gray-700" />
+								</button>
+							</div>
+							<div className="p-4 text-black">
+								<h2 className="text-xl font-bold">
+									{product.brand} {product.car_model}
+								</h2>
+								<p className="text-sm line-clamp-1">
+									{product.bio}
+								</p>
+								<div className="flex items-center justify-between mt-4 text-gray-700 mx-3">
+									<div className="items-center mr-4">
+										<RiSpeedUpFill className="w-5 h-5 mr-1" />
+										{product.performance.top_speed}
+									</div>
+									<div className="items-center mr-4">
+										<FaGasPump className="w-5 h-5 mr-1" />
+										{product.fuel_type}
+									</div>
+									<div className="items-center">
+										<FaCogs className="w-5 h-5 mr-1" />
+										{product.transmission}
+									</div>
 								</div>
-								<div className="items-center mr-4">
-									<FaGasPump className="w-5 h-5 mr-1" />
-									{product.fuel_type}
-								</div>
-								<div className="items-center">
-									<FaCogs className="w-5 h-5 mr-1" />
-									{product.transmission}
+								<div className="mt-4 flex justify-between items-center">
+									<span className="text-2xl font-bold text-gray-900">
+										{product.price + "$"}
+									</span>
+									<Link to="" className="text-blue-500 flex">
+										View Details
+										<MdArrowOutward className="w-5 h-5 ml-1" />
+									</Link>
 								</div>
 							</div>
-							<div className="mt-4 flex justify-between items-center">
-								<span className="text-2xl font-bold text-gray-900">
-									{product.price + "$"}
-								</span>
-								<Link to="" className="text-blue-500 flex">
-									View Details
-									<MdArrowOutward className="w-5 h-5 ml-1" />
-								</Link>
-							</div>
 						</div>
-					</div>
+					))}
+			</div>
+
+			{/* Pagination */}
+			<div className="flex justify-center mt-8">
+				{Array.from({ length: totalPages }, (_, i) => (
+					<button
+						key={i}
+						onClick={() => paginate(i + 1)}
+						className={`mx-1 px-3 py-1 rounded ${
+							currentPage === i + 1
+								? "bg-gray-600 text-white"
+								: "bg-gray-200"
+						}`}
+					>
+						{i + 1}
+					</button>
 				))}
-		</div>
+			</div>
+		</>
 	);
 };
 
