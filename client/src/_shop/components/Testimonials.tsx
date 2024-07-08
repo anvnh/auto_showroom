06b8ead00fc2d87affbe19a41b6662d4ce155799
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-07-09 00:09:51 +0700

    repon shop

--- client/src/_shop/components/Testimonials.tsx
+++ client/src/_shop/components/Testimonials.tsx
@@ -2,15 +2,15 @@ import { IoIosStar } from "react-icons/io";
 import { MdOutlineVerifiedUser } from "react-icons/md";
 const Testimonials = () => {
     return (
-        <div className="grid grid-cols-2">
+        <div className="grid sm:grid-cols-2 grid-cols-1">
             <div className="text-black">
-                <div className="px-60 pt-20">
-                    <div className="text-3xl font-bold font-poppins">
+                <div className="md:px-60 px-12 pt-20">
+                    <div className="text-3xl md:text-5xl font-bold font-poppins">
                         What Our <br /> Customers Say
                     </div>
 
                     <div className="py-12">
-                        <h1 className="font-bold text-xl pb-3">
+                        <h1 className="font-bold md:text-2xl pb-3">
                             Great
                         </h1>
                         <div className="flex text-green-400">
@@ -20,13 +20,13 @@ const Testimonials = () => {
                             <IoIosStar className="text-xl" />
                             <IoIosStar className="text-xl" />
                         </div>
-                        <div className="pt-3">
+                        <div className="pt-3 md:text-xl">
                             Based on 1000+ reviews
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="pt-20">
+            <div className="pt-12 md:pt-20 px-12">
                 <div className="flex text-green-400 text-3xl items-center">
                     <IoIosStar className="" />
                     <IoIosStar className="" />
@@ -35,9 +35,9 @@ const Testimonials = () => {
                     <IoIosStar className="" />
                     <MdOutlineVerifiedUser className="text-xl"/>
                 </div>
-                <div className="pt-5 text-black">
+                <div className="pt-5 md:text-xl text-black">
                     <p>
-                        <span className="font-bold">John Doe</span> <br />
+                        <span className="font-bold md:text-2xl">John Doe</span> <br />
                         <span>CEO at Company</span>
                     </p>
                     <p className="pt-5">
