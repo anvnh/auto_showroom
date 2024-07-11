d39c80d1b7cd33c78e2048e1b156f95fb06c6214
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-07-12 02:19:18 +0700

    update login signup

--- client/src/_social/pages/auth/login/LoginPage.tsx
+++ client/src/_social/pages/auth/login/LoginPage.tsx
@@ -3,12 +3,16 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { toast } from "react-hot-toast";
 import { useQueryClient } from "@tanstack/react-query";
-import { logo, bgSocial, gifSocial } from "@/assets";
+import { logo, bgSocial, gif, ngoisao } from "@/assets";
 import { Link } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
+import { FcGoogle } from "react-icons/fc";
+import LoginRepon from "./LoginRepon";
+
 
 const LoginPage: React.FC = () => {
 	const [showSignUpForm, setShowSignUpForm] = useState(false);
-	const [showSignInForm, setShowSignInForm] = useState(false);
+	const [showSignInForm, setShowSignInForm] = useState(true);
 	const [activeForm, setActiveForm] = useState<string>("");
 
 	const [signUpData, setSignUpData] = useState({
@@ -125,85 +129,41 @@ const LoginPage: React.FC = () => {
 	};
 
 	return (
-		<div className="w-full">
-			<div className="hidden xl:block">
-				<div
-					style={{ backgroundImage: `url(${bgSocial})` }}
-					className="w-full h-[923px] bg-center bg-cover duration-500 "
-				>
-					<div className="flex items-center min-h-screen col-span-1  md:col-span-2 gap-[200px]">
-						<div className="absolute top-0 left-0 w-full h-[670px] md:h-[923px] bg-gradient-to-r from-gray-800 to-transparent"></div>
+		<div className="w-full bg-primary">
+			<video
+				autoPlay
+				muted
+				loop
+				playsInline
+				className="fixed inset-0 w-full h-full object-cover z-0"
+			>
+				<source src={ngoisao} type="video/mp4" />
+			</video>
+			<div className="hidden xl:block ">
+				<div className="w-full h-auto bg-center bg-cover duration-500">
+					<div className="flex items-center min-h-screen col-span-1  md:col-span-2 gap-[200px] pt-12">
 						<div
 							data-aos="fade-right"
-							className="pl-2 md:pl-[10px] text-center absolute top-24 w-[1200px] h-[300px] rounded-[20px] font-bold"
+							className="pl-2 md:pl-[1px] text-center absolute top-12 w-[1200px] font-bold"
 						>
 							<div
 								data-aos="fade-right"
-								className="justify-center flex"
+								className="justify-center flex pt-5"
 							>
-							
-								<Link to="http://localhost:3000/">
-								<img
-									src={logo}
-									alt="logo"
-									className="md:w-[300px] w-[55px] md:h-[250px] h-[55px] bottom-12 flex relative"
-								/>
+								<Link to="/">
+									<img
+										src={logo}
+										alt="logo"
+										className="md:w-[400px] w-[55px] md:h-full h-[55px] bottom-2 flex relative"
+									/>
 								</Link>
 							</div>
-							<h1 className="text-sm md:text-7xl  text-center font-syncopate text-gray-300">
-								Welcome <br /> to Social AAP
+							<h1 className="text-sm md:text-8xl text-center font-syncopate text-gray-300">
+								Welcome <br /> to AAP
 							</h1>
 						</div>
 
-						<div className="pl-[250px] top-48 pt-16 relative w-[900px] h-[300px] rounded-[20px]">
-							<div className="absolute">
-								{showSignUpForm && (
-									<div
-										data-aos="fade-right"
-										className="text-center relative pl-[150px] text-2xl font-syncopate"
-									>
-										<p>You have an account ?</p>
-									</div>
-								)}
-
-								{showSignInForm && (
-									<div
-										data-aos="fade-right"
-										className="text-center relative pl-[50px] text-2xl font-syncopate"
-									>
-										<p>You don't have an account ?</p>
-									</div>
-								)}
-							</div>
-
-							<div
-								data-aos="fade-left"
-								className="pt-0 justify-center items-center flex"
-							>
-								<button
-									type="button"
-									onClick={() => toggleForm("signUp")}
-									className={`font-semibold w-[160px] h-[50px] text-white hover:bg-gray-600  transition-all ease-in-out duration-700 text-xl hover:rounded-3xl border-[1px] font-syncopate top-16 relative ${
-										activeForm === "signUp"
-											? "bg-gray-900 text-black rounded-3xl border-[1px]"
-											: ""
-									}`}
-								>
-									Sign Up
-								</button>
-								<button
-									type="button"
-									onClick={() => toggleForm("signIn")}
-									className={`font-semibold w-[160px] h-[50px] text-white hover:bg-gray-600 transition-all ease-in-out duration-700 text-xl hover:rounded-3xl border-[1px] font-syncopate top-16 ml-4 relative ${
-										activeForm === "signIn"
-											? "bg-gray-900 text-black rounded-3xl border-[1px]"
-											: ""
-									}`}
-								>
-									Sign In
-								</button>
-							</div>
-						</div>
+						<div className="pl-[250px] top-48 pt-16 relative w-[900px] h-[300px] rounded-[20px]"></div>
 
 						<div
 							data-aos="fade-right"
@@ -212,12 +172,12 @@ const LoginPage: React.FC = () => {
 							{showSignUpForm && (
 								<div
 									data-aos="fade-left"
-									className={`relative z-10 w-[500px] p-20 bg-black bg-opacity-50 shadow-xl rounded-lg rounded-tr-[200px] rounded-bl-[200px] mr-10 `}
+									className={`relative z-10 w-[500px] pt-20 px-20 pb-5 bg-primary bg-opacity-50 shadow-md shadow-white rounded-tr-[200px] rounded-xl mr-10 backdrop-blur-md`}
 								>
-									<div className="absolute w-[400px] h-[70px] -top-[220px] z-40 -left-[170px] transform scale-x-[-1]">
-									<img src={gifSocial}  />
+									<div className="absolute w-[400px] h-[70px] -top-[160px] z-40 -left-[170px] transform scale-x-[-1]">
+										<img src={gif} />
 									</div>
-									<h2 className="text-center pb-12 text-4xl font-syncopate text-white mb-6">
+									<h2 className="text-center pb-12 text-5xl font-poppins text-white mb-6">
 										Sign Up
 									</h2>
 									<form
@@ -262,7 +222,7 @@ const LoginPage: React.FC = () => {
 												/>
 												<label
 													className={`absolute -left-12 top-6 text-gray-400 text-base peer-focus:-top-3 
-													peer-focus:left-12	peer-focus:text-sm transition-all duration-300 ${
+													peer-focus:left-12 peer-focus:text-sm transition-all duration-300 ${
 														signUpData[
 															placeholder as keyof typeof signUpData
 														]
@@ -277,13 +237,26 @@ const LoginPage: React.FC = () => {
 												</label>
 											</div>
 										))}
-										<div className="pt-12 relative">
-											<button
-												type="submit"
-												className="w-full h-[50px] p-2 text-white bg-gray-500 hover:bg-gray-700 transition-all ease-in-out rounded-md font-syncopate duration-500"
-											>
-												Create Account
-											</button>
+
+										<div className="pt-10 relative flex justify-center">
+											<Button className="w-[300px] h-[50px] p-2 text-white bg-gray-500 hover:bg-gray-700 transition-all ease-in-out rounded-md text-md font-poppins duration-500">
+												C<span className="lowercase">reate Account</span>
+											</Button>
+										</div>
+										<div className="flex pt-12 justify-center items-center text-center">
+											<p>
+												You have an account
+											</p>
+											<div className="w-[70px]">
+												<a
+													onClick={() =>
+														toggleForm("signIn")
+													}
+													className={`cursor-pointer text-blue-300 transition-all ease-in-out duration-700 text-sm font-poppins relative hover:underline`}
+												>
+													Sign In
+												</a>
+											</div>
 										</div>
 									</form>
 								</div>
@@ -292,16 +265,16 @@ const LoginPage: React.FC = () => {
 							{showSignInForm && (
 								<div
 									data-aos="fade-left"
-									className={`relative z-10 w-[500px] p-20 bg-black bg-opacity-50 shadow-xl rounded-lg rounded-tr-[200px] rounded-bl-[200px] mr-10 `}
+									className={`relative z-10 w-[500px] pt-20 px-20 pb-5 bg-primary shadow-md shadow-white bg-opacity-50 rounded-xl rounded-tr-[200px] mr-10 backdrop-blur-md`}
 								>
-										<div className="absolute w-[400px] h-[70px] -top-[220px] z-40 -left-[170px] transform scale-x-[-1]">
-										<img src={gifSocial}  />
+									<div className="absolute w-[400px] h-[70px] -top-[160px] z-40 -left-[170px] transform scale-x-[-1]">
+										<img src={gif} />
 									</div>
-									<h2 className="text-center pb-12 text-4xl font-syncopate text-white mb-6">
+									<h2 className="text-center pb-12 text-5xl font-poppins text-white mb-6">
 										Sign In
 									</h2>
 									<form
-										className="space-y-6 pl-12"
+										className="space-y-6"
 										onSubmit={handleSignInSubmit}
 										noValidate
 									>
@@ -309,7 +282,7 @@ const LoginPage: React.FC = () => {
 											(placeholder, index) => (
 												<div
 													key={index}
-													className="relative"
+													className="relative pl-12"
 												>
 													<input
 														type={
@@ -335,7 +308,7 @@ const LoginPage: React.FC = () => {
 													/>
 													<label
 														className={`absolute -left-24 top-6 text-gray-400 text-base peer-focus:-top-3 
-													peer-focus:left-0	peer-focus:text-sm transition-all duration-300 ${
+													peer-focus:left-0 peer-focus:text-sm transition-all duration-300 ml-12 ${
 														signUpData[
 															placeholder as keyof typeof signUpData
 														]
@@ -353,13 +326,54 @@ const LoginPage: React.FC = () => {
 												</div>
 											)
 										)}
-										<div className="pt-12 relative">
-											<button
+										<div
+											data-aos="fade-left"
+											className="pt-0 pb-1 w-full justify-center z-10 items-center flex gap-5"
+										>
+											<a className="w-full h-5 cursor-pointer flex justify-end  text-blue-300 transition-all ease-in-out duration-700 text-sm font-poppins z-50 relative hover:underline ">
+												Recovery Password
+											</a>
+										</div>
+										<div className="pt-2 relative flex justify-center">
+											<Button
 												type="submit"
-												className="w-full h-[50px] p-2 text-white bg-gray-500 hover:bg-gray-700 transition-all ease-in-out rounded-md font-syncopate duration-500"
+												className="w-[300px] h-[50px] text-white bg-gray-500 hover:bg-gray-700 transition-all ease-in-out rounded-md text-md font-poppins duration-500"
 											>
-												Sign In
-											</button>
+												S
+												<span className="lowercase">
+													ign In
+												</span>
+											</Button>
+										</div>
+										<div className="relative flex justify-center">
+											<Button
+												type="submit"
+												className="flex items-center justify-center w-[300px] h-[50px] text-white bg-gray-900 hover:bg-gray-700 transition-all ease-in-out rounded-md text-md font-poppins duration-500"
+											>
+												<FcGoogle className="mr-2 h-auto w-[25px]" />
+												<div className="flex items-center">
+													<span className="lowercase font-bold">
+														Sign In with
+													</span>
+													<span className="ml-1 lowercase">
+														Google
+													</span>
+												</div>
+											</Button>
+										</div>
+
+										<div className="flex pt-12 justify-center items-center text-center">
+											<p>Don't have an account yet?</p>
+											<div className="w-[70px]">
+												<a
+													onClick={() =>
+														toggleForm("signUp")
+													}
+													className={`cursor-pointer text-blue-300 transition-all ease-in-out duration-700 text-sm font-poppins relative hover:underline`}
+												>
+													Sign Up
+												</a>
+											</div>
 										</div>
 									</form>
 								</div>
@@ -370,189 +384,7 @@ const LoginPage: React.FC = () => {
 			</div>
 
 			<div className="xl:hidden">
-				<div
-					style={{ backgroundImage: `url(${bgSocial})` }}
-					className="w-full h-auto relative bg-cover bg-center"
-				>
-					<div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-gray-800 to-transparent"></div>
-
-					<div className="relative flex flex-col items-center justify-center min-h-screen gap-8 px-4">
-						<div className="flex flex-col items-center">
-							
-							<Link to="http://localhost:3000/">
-							<img
-								src={logo}
-								alt="logo"
-								className="md:w-[300px] w-[80px] md:h-[200px] h-[70px]"
-							/>
-								</Link>
-							<h1 className="text-xl md:text-8xl text-center font-syncopate text-gray-300 shadow-2xl mt-1">
-								Welcome to Social AAP
-							</h1>
-						</div>
-
-						{/* Button Section */}
-						<div className="w-full max-w-md">
-							<div className="flex flex-col items-center space-y-4">
-								{showSignUpForm && (
-									<div className="text-center font-syncopate">
-										<p>You have an account?</p>
-									</div>
-								)}
-								{showSignInForm && (
-									<div className="text-center font-syncopate">
-										<p>You don't have an account?</p>
-									</div>
-								)}
-
-								<div className="flex justify-center space-x-4">
-									<button
-										type="button"
-										onClick={() => toggleForm("signUp")}
-										className={`font-semibold w-[160px] h-[50px] text-white hover:bg-gray-600 transition-all ease-in-out duration-700 text-xl rounded-3xl border-[1px] font-syncopate ${
-											activeForm === "signUp"
-												? "bg-gray-900 text-black border-[1px]"
-												: ""
-										}`}
-									>
-										Sign Up
-									</button>
-									<button
-										type="button"
-										onClick={() => toggleForm("signIn")}
-										className={`font-semibold w-[160px] h-[50px] text-white hover:bg-gray-600 transition-all ease-in-out duration-700 text-xl rounded-3xl border-[1px] font-syncopate ${
-											activeForm === "signIn"
-												? "bg-gray-900 text-black border-[1px]"
-												: ""
-										}`}
-									>
-										Sign In
-									</button>
-								</div>
-							</div>
-						</div>
-						{/* 
-                            ----------------- */}
-						<div className="flex justify-center sm:px-12 w-full">
-							{showSignUpForm && (
-								<div className="w-full p-8 bg-black bg-opacity-50 shadow-xl rounded-lg">
-									<form
-										className="space-y-6"
-										onSubmit={handleSignUpSubmit}
-										noValidate
-									>
-										{[
-											"email",
-											"username",
-											"fullName",
-											"password",
-										].map((placeholder, index) => (
-											<div
-												key={index}
-												className="relative"
-											>
-												<input
-													placeholder={
-														placeholder
-															.charAt(0)
-															.toUpperCase() +
-														placeholder.slice(1)
-													}
-													type={
-														placeholder ===
-														"password"
-															? "password"
-															: placeholder ===
-															  "email"
-															? "email"
-															: "text"
-													}
-													name={placeholder}
-													value={
-														signUpData[
-															placeholder as keyof typeof signUpData
-														]
-													}
-													onChange={(e) =>
-														handleInputChange(
-															e,
-															"signUp"
-														)
-													}
-													className="w-full p-2 text-white bg-transparent border-b-2 border-white focus:outline-none peer"
-													required
-												/>
-											</div>
-										))}
-										<div className="pt-6 ">
-											<button
-												type="submit"
-												className="w-full h-[50px] p-2 text-white bg-gray-500 hover:bg-gray-700 transition-all ease-in-out rounded-md font-syncopate duration-500"
-											>
-												Create Account
-											</button>
-										</div>
-									</form>
-								</div>
-							)}
-							{showSignInForm && (
-								<div className="w-full max-w-md p-8 bg-black bg-opacity-50 shadow-xl rounded-lg">
-									<form
-										className="space-y-6"
-										onSubmit={handleSignInSubmit}
-										noValidate
-									>
-										{["username", "password"].map(
-											(placeholder, index) => (
-												<div
-													key={index}
-													className="relative"
-												>
-													<input
-														placeholder={
-															placeholder
-																.charAt(0)
-																.toUpperCase() +
-															placeholder.slice(1)
-														}
-														type={
-															placeholder ===
-															"password"
-																? "password"
-																: "text"
-														}
-														name={placeholder}
-														value={
-															signInData[
-																placeholder as keyof typeof signInData
-															]
-														}
-														onChange={(e) =>
-															handleInputChange(
-																e,
-																"signIn"
-															)
-														}
-														className="w-full p-2 text-white bg-transparent border-b-2 border-white focus:outline-none peer"
-														required
-													/>
-												</div>
-											)
-										)}
-										<div className="pt-6">
-											<button
-												type="submit"
-												className="w-full h-[50px] p-2 text-white bg-gray-500 hover:bg-gray-700 transition-all ease-in-out rounded-md font-syncopate duration-500"
-											>
-												Log in
-											</button>
-										</div>
-									</form>
-								</div>
-							)}
-						</div>
-					</div>
-				</div>
+					<LoginRepon />		
 			</div>
 		</div>
 	);
