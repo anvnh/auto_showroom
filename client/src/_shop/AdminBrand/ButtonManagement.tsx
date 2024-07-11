eb435e14994332c47c0801cec98b199ea48815f8
anvnh <anvo20052@gmail.com>
2024-07-11 21:07:31 +0700

    Change apperance of button in admin view

--- client/src/_shop/AdminBrand/ButtonManagement.tsx
+++ client/src/_shop/AdminBrand/ButtonManagement.tsx
@@ -1,26 +1,27 @@
-import React from "react";
 import { Button } from "@material-tailwind/react";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { CiCircleMinus } from "react-icons/ci";
 import { FiRefreshCw } from "react-icons/fi";
 
 const ButtonManagement = () => {
+
 	return (
-		<div className="p-5 space-y-5">
-			<Button className="bg-gradient-to-b from-gray-700 to-gray-400 text-white w-56 h-10 text-md items-center flex justify-start">
+		<div className="p-5 space-y-3">
+			<Button
+				className="bg-primary bg-opacity-50 text-white w-56 h-10 text-md items-center flex justify-start"
+			>
 				<IoIosAddCircleOutline />
 				<p className="pl-4">Add product</p>
 			</Button>
 
-			<Button className="bg-gradient-to-b from-gray-700 to-gray-400 text-white w-56 h-10 text-md items-center flex justify-start">
+			<Button className="bg-primary bg-opacity-50 text-white w-56 h-10 text-md items-center flex justify-start">
 				<CiCircleMinus />
-				<p className="pl-4">Delete product</p>
+				<p className="pl-4"> Delete product </p>
 			</Button>
 
-			<Button className="bg-gradient-to-b from-gray-700 to-gray-400 text-white w-56 h-10 text-md items-center flex justify-start">
+			<Button className="bg-primary bg-opacity-50 text-white w-56 h-10 text-md items-center flex justify-start">
 				<FiRefreshCw />
 				<p className="pl-4">Update product</p>
-                
 			</Button>
 		</div>
 	);
