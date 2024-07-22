f915304d5c45577e9f2975cc4276adf2e5decab9
anvnh <anvo20052@gmail.com>
2024-07-22 18:40:36 +0700

    Minor fix

--- server/server.js
+++ server/server.js
@@ -24,10 +24,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
-
-
-
+// const __dirname = path.resolve();
 
 app.use(cors(
 )); // to allow cross-origin requests
@@ -44,12 +41,12 @@ app.use("/api/posts", postRoutes); // post routes
 app.use("/api/notifications", nofiticationRoutes); // notification routes
 app.use("/api/car", carRoutes); // car routes
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-    })
-}
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/dist")));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+//     })
+// }
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
