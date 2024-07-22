f950a7b0c73eb714db26e891632d0ab749f717a5
Võ Nguyễn Hoàng An <anvo20052@gmail.com>
2024-07-22 20:58:52 +0700

    Update index.js

--- server_deploy/index.js
+++ server_deploy/index.js
@@ -5,15 +5,10 @@ const RegisterModel = require('./models/Register')
 
 const app = express()
 app.use(cors(
-    {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://admin:1@cluster.s2dwrtl.mongodb.net/?retryWrites=true&w=majority&appName=cluster');
 
 
 app.get("/", (req, res) => {
