09b5beeee50197b6850e5e310768ca8ee931b5be
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-08-02 11:22:43 -0300

    repon shop, product, fix payment

--- client/src/_shop/products/Products.tsx
+++ client/src/_shop/products/Products.tsx
@@ -1,22 +1,24 @@
 import LoadingSpinner from "@/components/social/ui/common/LoadingSpinner";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { FaBookmark } from "react-icons/fa";
+import { FaBookmark, FaSearch } from "react-icons/fa";
 import SearchBar from "../common/SearchBar";
 import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import calculateAvgRating from "@/utils/calculateAvgRating";
+import axios from "axios";
+import { Input } from "@/components/ui/input"
 
 const Products = () => {
-	const [currentPage, setCurrentPage] = useState(1);
-	const productsPerPage = 4;
 
-	// get all products
-	const {
-		data: products,
-		isLoading,
-		refetch,
-		isRefetching,
-	} = useQuery({
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 4;
+
+	const [loadingProductId, setLoadingProductId] = useState(null);
+
+    // get all products
+	const { data: products, isLoading, refetch, isRefetching} = useQuery({
 		queryKey: ["products"],
 		queryFn: async () => {
 			try {
@@ -35,122 +37,183 @@ const Products = () => {
 			}
 		},
 	});
+	// add to cart
+	const {mutate: addToCart, isPending,} = useMutation({
+		mutationFn: async (productId) => {
+			try {
+				const response = await fetch(`/api/user/add/cart/${productId}`, {
+					method: "POST",
+				});
+				const data = await response.json();
 
-	// calculate
-	const indexOfLastProduct = currentPage * productsPerPage;
+				if (!response.ok) {
+					throw new Error(data.error || "Something went wrong!");
+				}
+
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Product added to cart", {
+				duration: 2000, 
+			});
+
+			setTimeout(() => { setLoadingProductId (null) });
+		},
+		onError: (error) => {
+			// TODO
+			toast.error("Item already in cart", {
+				duration: 2000,
+			});
+
+			setTimeout(() => { setLoadingProductId (null) });
+		}
+    });
+
+	const [cars, setCars] = useState([]);
+
+	const handleSearch = async ( searchTerm: string) => {
+        try {
+            const response = await axios.get(`/api/car/?search=${searchTerm}`);
+            setCars(response.data);
+			// console.log(response.data);
+        } catch (error) {
+            console.error("Error fetching cars:", error);
+        }
+    };
+
+	const handleAddToCart = (productId) => {
+		setLoadingProductId(productId);
+		addToCart(productId);
+	}
+ 
+    // calculate
+    const indexOfLastProduct = currentPage * productsPerPage;
 	const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-	const currentProducts = products
-		? products.slice(indexOfFirstProduct, indexOfLastProduct)
-		: [];
-	const totalPages = products
-		? Math.ceil(products.length / productsPerPage)
-		: 0;
+	// const currentProducts = products ? products.slice(indexOfFirstProduct, indexOfLastProduct) : [];
+	const currentProducts = cars.length > 0 ? cars.slice(indexOfFirstProduct, indexOfLastProduct) : (products ? products.slice(indexOfFirstProduct, indexOfLastProduct) : []);
+	// const totalPages = products ? Math.ceil(products.length / productsPerPage) : 0;
+	const totalPages = cars.length > 0 ? Math.ceil(cars.length / productsPerPage) : (products ? Math.ceil(products.length / productsPerPage) : 0);
 
 	const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-	const handleSearch = (searchTerm: string) => {
-		console.log("Searching for:", searchTerm);
-	};
-
-	return (
-		<section className="xl:p-4 w-full">
-			<div className="md:block hidden">
-				<div className="flex w-full justify-end mb-6">
-					<SearchBar onSearch={handleSearch} />
+    return (
+		<section className="p-4">
+			<div className="flex w-full justify-end mb-3 px-3">
+				<div className="text-[16px] flex border border-white justify-between items-center p-2 space-x-0 rounded-xl">
+					<input 
+						placeholder="Search" className="ml-2 bg-primary w-[220px] border-none focus:outline-none focus:border-none focus:ring-0"
+						onChange={(e) => handleSearch(e.target.value)}
+					/>
+					<FaSearch className="w-4 h-4 text-white cursor-pointer" title="Search"/>
 				</div>
 			</div>
-			<div className="grid md:grid-cols-5 gap-1">
-				<div className="col-span-1">
+			<div className="flex">
+				<div className="">
 					<Sidebar />
 				</div>
-				<div className="col-span-4 p-3">
-					<div className="block md:hidden mb-6">
-						<SearchBar onSearch={handleSearch} />
-					</div>
-					{isLoading && !products && (
+				<div 
+					className="p-3"
+				>
+					{isLoading && products && products.length === 0 && (
 						<div className="text-center text-2xl font-bold text-gray-700">
 							There are no products available at the moment.
 						</div>
 					)}
 					{!isLoading &&
 						!isRefetching &&
-						currentProducts &&
-						currentProducts.map((product) => (
-							<Link
-								to={`/shop/product/${product._id}`}
-								key={product._id}
-							>
-								<div className="md:flex bg-white hover:bg-opacity-90 p-4 mb-4 rounded-2xl shadow-md w-full h-auto">
-									<div className="relative w-full md:w-1/3 mr-4 overflow-hidden items-center flex">
-										<img
-											src={product.images[0]}
-											className="w-full md:h-[250px] rounded"
-										/>
-									</div>
-									<div className="w-full md:w-2/3">
-										<h2 className="text-xl font-bold mb-2 text-black">
-											{product.brand}&nbsp;
-											{product.car_model}
-										</h2>
-										<div className="md:flex items-center mb-2">
-											<div className="flex text-yellow-400">
-												{"★".repeat(5)}
-												{"☆".repeat(5 - 5)}
-											</div>
-											<span className="text-gray-600 text-sm ml-2">
-												0 reviews
-											</span>
-											<a
-												href="#"
-												className="text-blue-500 text-sm ml-2"
+						currentProducts?.map((product) => {
+							const averageRating = calculateAvgRating({
+								reviews: product.user_review,
+							});
+							return (
+								<>
+									<Toaster position="top-center" reverseOrder={false} />
+									<div
+										key={product._id}
+										className="flex bg-white hover:bg-opacity-90 p-4 mb-4 rounded-2xl shadow-md w-full h-[300px]"
+									>
+										<div className="w-1/3 mr-4 overflow-hidden items-center flex">
+											<Link
+												to={`/shop/product/${product._id}`}
+												className="w-full h-[250px] rounded"
 											>
-												Submit a review
-											</a>
+												<img
+													src={product.images[0]}
+													className="w-full h-[250px] rounded"
+												/>
+											</Link>
 										</div>
-										<div className="mb-2">
-											<span className="text-2xl font-bold text-blue-600">
-												$&nbsp;{product.price}
-											</span>
-										</div>
-										<div className="xl:block hidden">
-											<p className="text-gray-700 mb-4">
-												{product.bio.length > 320
-													? `${product.bio.substring(
-															0,
-															310
-													  )}...`
-													: product.bio}
+										<div className="w-2/3">
+											<h2 className="text-xl font-bold mb-2 text-black">
+												{product.brand}&nbsp;
+												{product.car_model}
+											</h2>
+											<div className="flex items-center mb-1">
+												<div className="flex text-xl text-yellow-400">
+													{"★".repeat(
+														Math.round(
+															averageRating
+														)
+													)}
+													{"☆".repeat(
+														5 -
+															Math.round(
+																averageRating
+															)
+													)}
+												</div>
+												<span className="text-gray-600 text-sm ml-2">
+													{product.user_review.length}{" "}
+													reviews
+												</span>
+											</div>
+											<div className="mb-2">
+												<span className="text-2xl font-bold text-blue-600">
+													$&nbsp;{product.price}
+												</span>
+											</div>
+											<p className="text-gray-700 mb-4 line-clamp-4">
+												{product.bio}
 											</p>
-										</div>
-										<div className="block xl:hidden">
-											<p className="text-gray-700 mb-4">
-												{product.bio.length > 320
-													? `${product.bio.substring(
-															0,
-															100
-													  )}...`
-													: product.bio}
-											</p>
-										</div>
-										<div className="flex gap-4">
-											<button className="detail-button bg-gray-300 text-black px-4 py-2 md:px-6 md:py-3 w-[150px] lg:w-[170px] lg:h-[40px] items-center justify-center flex hover:bg-black transition-all duration-300 ease-in-out hover:text-white font-bold text-sm md:text-base rounded-xl text-center relative h-9  overflow-hidden border-gray-600 border shadow-2xl before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-12 before:bg-white before:opacity-50 before:duration-700 hover:shadow-gray-500 font-poppins hover:before:-translate-x-[210px] ">
-												Add To Cart
-											</button>
-											<button className="border border-gray-300 text-gray-700 px-4 py-2 rounded">
-												<FaBookmark />
-											</button>
+											<div className="flex">
+												<button
+													className="h-[40px] bg-blue-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-600"
+													onClick={() =>
+														handleAddToCart(
+															product._id
+														)
+													}
+												>
+													{loadingProductId ===
+													product._id ? (
+														<LoadingSpinner />
+													) : (
+														<span>
+															{" "}
+															Add to cart{" "}
+														</span>
+													)}
+												</button>
+												<button className="border border-gray-300 text-gray-700 px-4 py-2 rounded">
+													<FaBookmark />
+												</button>
+											</div>
 										</div>
 									</div>
-								</div>
-							</Link>
-						))}
+								</>
+							);
+						})}
 					<div>
+						{/* Pagination */}
 						<div className="flex justify-center mt-8">
 							{Array.from({ length: totalPages }, (_, i) => (
 								<button
 									key={i}
 									onClick={() => paginate(i + 1)}
-									className={`mx-1 px-3 py-1 rounded transition-all duration-300 ${
+									className={`mx-1 px-3 py-1 rounded transision-all duration-300 ${
 										currentPage === i + 1
 											? "bg-gray-600 text-white scale-110"
 											: "bg-gray-200 hover:bg-gray-300"
@@ -165,6 +228,6 @@ const Products = () => {
 			</div>
 		</section>
 	);
-};
+}
 
-export default Products;
+export default Products
