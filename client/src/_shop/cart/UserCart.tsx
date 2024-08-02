822e7a4bfda46f20bc6d4268b60cd3d54aaf88bd
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-08-02 04:08:27 -0300

    done payment, repon shop/cart

--- client/src/_shop/cart/UserCart.tsx
+++ client/src/_shop/cart/UserCart.tsx
@@ -2,41 +2,45 @@ import LoadingSpinner from "@/components/social/ui/common/LoadingSpinner";
 import calculateAvgRating from "@/utils/calculateAvgRating";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-import { MdAddShoppingCart, MdDelete } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 import QuantityCounter from "@/utils/QuantityCounter";
 import { toast, Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
-import OverviewCart from "../payment/OverviewCart";
-
+import { Link, createSearchParams } from "react-router-dom";
 
 const UserCart = () => {
+	const queryClient = useQueryClient();
 
-    const queryClient = useQueryClient();
-
-	const [deletingItems, setDeletingItems] = useState<{ [key: string]: boolean }>({});
+	const [deletingItems, setDeletingItems] = useState<{
+		[key: string]: boolean;
+	}>({});
 
 	const [cartInfo, setCartInfo] = useState({ items: [], total: 0 });
 
-    const {data: cart, isLoading, refetch, isRefetching} = useQuery({
-        queryKey: ["cart"],
-        queryFn: async () => {
-            try {
-                const response = await fetch("/api/user/cart");
-                const data = await response.json();
+	const {
+		data: cart,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["cart"],
+		queryFn: async () => {
+			try {
+				const response = await fetch("/api/user/cart");
+				const data = await response.json();
 
-                if (!response.ok) {
-                    throw new Error(data.error || "Something went wrong!");
-                }
+				if (!response.ok) {
+					throw new Error(data.error || "Something went wrong!");
+				}
 
-                return data;
-            } catch (error) {
-                throw new Error(error);
-            }
-        },
-    });
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
 
-    const { mutate: deleteItem, isPending: isDeleting} = useMutation({
-		mutationFn: async ({item_id}) => {
+	const { mutate: deleteItem, isPending: isDeleting } = useMutation({
+		mutationFn: async ({ item_id }) => {
 			try {
 				const res = await fetch(`/api/user/delete/cart/${item_id}`, {
 					method: "DELETE",
@@ -46,200 +50,295 @@ const UserCart = () => {
 				});
 				const data = await res.json();
 
-				if(!res.ok){
+				if (!res.ok) {
 					throw new Error(data.error || "Something went wrong");
 				}
 
 				return data;
-			}
-			catch (error) {
+			} catch (error) {
 				throw new Error(error);
 			}
 		},
 		onSuccess: (data, variables) => {
 			toast.success("Car removed successfully");
 			// invalidate the query to refetch the data
-			setDeletingItems((prev) => ({ ...prev, [variables.item_id]: false }));
-			queryClient.invalidateQueries({queryKey: ["cart"]});
+			setDeletingItems((prev) => ({
+				...prev,
+				[variables.item_id]: false,
+			}));
+			queryClient.invalidateQueries({ queryKey: ["cart"] });
 		},
 		onError: (error, variables) => {
-            setDeletingItems((prev) => ({ ...prev, [variables.item_id]: false }));
-            toast.error("Failed to remove item");
-        },
+			setDeletingItems((prev) => ({
+				...prev,
+				[variables.item_id]: false,
+			}));
+			toast.error("Failed to remove item");
+		},
 	});
 
 	const calculateTotalPrice = () => {
-        if (!cart) return 0;
-        return cart.reduce((total, item) => {
-            const itemTotal = Number(item.price.replace(/,/g, "")) * (quantities[item._id] || 1);
-            return total + itemTotal;
-        }, 0).toLocaleString();
-    };
+		if (!cart) return 0;
+		return cart
+			.reduce((total, item) => {
+				const itemTotal =
+					Number(item.price.replace(/,/g, "")) *
+					(quantities[item._id] || 1);
+				return total + itemTotal;
+			}, 0)
+			.toLocaleString();
+	};
 
-    const [quantities, setQuantities] = useState({});
+	const [quantities, setQuantities] = useState({});
 
-    const increaseQuantity = (item) => {
-        setQuantities((prevQuantities) => ({
-            ...prevQuantities,
-            [item._id]: (prevQuantities[item._id] || 1) + 1,
-        }));
-    };
+	const increaseQuantity = (item) => {
+		setQuantities((prevQuantities) => ({
+			...prevQuantities,
+			[item._id]: (prevQuantities[item._id] || 1) + 1,
+		}));
+	};
 
-    const decreaseQuantity = (item) => {
-        setQuantities((prevQuantities) => ({
-            ...prevQuantities,
-            [item._id]: Math.max((prevQuantities[item._id] || 1) - 1, 1),
-        }));
-    };
+	const decreaseQuantity = (item) => {
+		setQuantities((prevQuantities) => ({
+			...prevQuantities,
+			[item._id]: Math.max((prevQuantities[item._id] || 1) - 1, 1),
+		}));
+	};
 
-  
 	// const handleDelete = async (itemId) => {
 	// 	deleteItem({item_id: itemId});
 	// };
 	const handleDelete = async (itemId) => {
-        setDeletingItems((prev) => ({ ...prev, [itemId]: true }));
-        deleteItem({ item_id: itemId });
-    };
+		setDeletingItems((prev) => ({ ...prev, [itemId]: true }));
+		deleteItem({ item_id: itemId });
+	};
 
-
-
-    return (
-		<section>
+	return (
+		<section className="pb-24 pt-12 xl:px-0 px-3 text-white">
 			<Toaster position="top-center" reverseOrder={false} />
-			<div className="bg-primary pt-0 md:px-0 px-5">
-				<h1 className="sm:text-4xl ss:text-4xl text-3xl font-bold mb-6 md:pt-9 pt-10">
-					Your cart
+
+			<div className="container xl:w-[1500px] pb-12 p-4 bg-gray-600 bg-opacity-30 backdrop-blur-md rounded-3xl">
+				<h1 className="text-5xl text-center font-bold mb-8 px-12 pt-12">
+					Your Cart
 				</h1>
-				<h2 className="sm:text-xl ss:text-xl text-md mb-4">
+				<h2 className="sm:text-xl text-center ss:text-xl px-12 text-md mb-12">
 					Remember to apply the coupon code if you have one !!!
 				</h2>
-				{isLoading && isRefetching && <LoadingSpinner />}
-				{!isLoading && !isRefetching && cart && cart.length === 0 && (
-					<div className="text-center text-xl text-white mt-24">
-						Your cart is empty 🙄. Add some cars to your cart
-					</div>
-				)}
-				{!isLoading &&
-					!isRefetching &&
-					cart &&
-					cart.map((item) => {
-						const averageRating = calculateAvgRating({
-							reviews: item.user_review,
-						});
-						return (
-							<section className="mb-4" key={item._id}>
-								<div className="flex space-x-44 text-white text-xl w-full justify-center ml-64 mb-2">
-									<ul className="li">Price</ul>
-									<ul className="li">Quantity</ul>
-									<ul className="li">Total</ul>
-								</div>
-								<div
-									key={item._id}
-									className="flex bg-white p-4 mb-4 rounded-2xl shadow-md w-full h-[300px] hover:bg-opacity-90"
-								>
-									<div className="relative w-1/3 overflow-hidden flex items-center">
-										<img
-											src={item.images[0]}
-											className="w-[400px] h-[250px] rounded"
-										/>
-									</div>
+				<Toaster position="top-center" reverseOrder={false} />
+				<div className="pt-0 px-5">
+					{isLoading && isRefetching && <LoadingSpinner />}
+					{!isLoading &&
+						!isRefetching &&
+						cart &&
+						cart.length === 0 && (
+							<div className="text-center text-xl text-white mt-24">
+								Your cart is empty 🙄. Add some cars to your
+								cart
+							</div>
+						)}
+					{!isLoading && !isRefetching && cart && (
+						<div className="h-auto xl:w-[1400px]">
+							{cart.map((item, index) => {
+								const averageRating = calculateAvgRating({
+									reviews: item.user_review,
+								});
+								return (
+									<section className="mb-4" key={item._id}>
+										<div className="hidden md:block">
+											<div className="flex flex-col md:flex-row space-x-24 text-white text-xl w-full pr-20 justify-end  mb-2">
+												<ul className="li">Price</ul>
+												<ul className="li">Quantity</ul>
+												<ul className="li">Total</ul>
+											</div>
+										</div>
 
-									<div className="w-2/3 text-black">
-										<div className="ml-56 flex">
-											<div className="mb-2 w-[100px]">
-												<span className="text-[18px] font-bold text-blue-600">
-													${item.price}
-												</span>
+										<div className="flex flex-col  md:flex-row bg-white p-4 mb-4 rounded-2xl shadow-md h-full hover:bg-opacity-90">
+											<div className="relative  flex items-center">
+												<img
+													src={item.images[0]}
+													className="xl:w-[700px] w-full h-full xl:h-[300px] rounded"
+												/>
 											</div>
 
-											<div className="ml-32 mb-2 text-black">
-												<div className="flex items-center hover:bg-gray-100 hover:bg-opacity-15 rounded-lg overflow-hidden w-24">
-													<QuantityCounter
-														quantity={
-															quantities[
-																item._id
-															] || 1
-														}
-														onIncrease={() =>
-															increaseQuantity(
-																item
+											<div className="w-full text-black">
+												<div className="hidden md:block">
+													<div className=" flex flex-col justify-end pr-3 gap-[55px]  md:flex-row">
+														<div className="mb-2 w-full md:w-[100px]">
+															<span className="text-[18px] font-bold text-blue-600">
+																${item.price}
+															</span>
+														</div>
+
+														<div className=" mb-2 text-black">
+															<div className="flex items-center hover:bg-gray-100 hover:bg-opacity-15 rounded-lg overflow-hidden w-full md:w-24">
+																<QuantityCounter
+																	quantity={
+																		quantities[
+																			item
+																				._id
+																		] || 1
+																	}
+																	onIncrease={() =>
+																		increaseQuantity(
+																			item
+																		)
+																	}
+																	onDecrease={() =>
+																		decreaseQuantity(
+																			item
+																		)
+																	}
+																/>
+															</div>
+														</div>
+
+														<div className=" mb-2 w-full md:w-[100px]">
+															<span className="text-[18px] font-bold text-blue-600">
+																$
+																{(
+																	Number(
+																		item.price.replace(
+																			/,/g,
+																			""
+																		)
+																	) *
+																	(quantities[
+																		item._id
+																	] || 1)
+																).toLocaleString()}
+															</span>
+														</div>
+													</div>
+												</div>
+
+												<h2 className="md:text-4xl text-xl pl-3 md:pt-0 pt-5 font-bold mb-2 text-black">
+													{item.brand}&nbsp;
+													{item.car_model}
+												</h2>
+
+												<div className="flex pl-3 text-2xl text-yellow-600 cursor-pointer">
+													{"★".repeat(
+														Math.round(
+															averageRating
+														)
+													)}
+													{"☆".repeat(
+														5 -
+															Math.round(
+																averageRating
 															)
-														}
-														onDecrease={() =>
-															decreaseQuantity(
-																item
+													)}
+												</div>
+
+												<h3 className="line-clamp-2 pl-3 md:px-5 md:text-xl mb-5">
+													{item.bio}
+												</h3>
+											</div>
+											<div className="xl:hidden block">
+												<div className="w-full justify-center flex">
+													<hr className=" border-black w-1/2 border-1" />
+												</div>
+											</div>
+											<div className="block md:hidden">
+												<div className="ml-3 mt-4 md:ml-12 ">
+													<div className="mb-2 w-full text-black md:w-[100px]">
+														Price:
+														<span className="text-[18px] ml-20 font-bold text-blue-600">
+															${item.price}
+														</span>
+													</div>
+
+													<div className="ml-0 md:ml-7 mb-2 text-black">
+														<div className="flex items-center hover:bg-gray-100 hover:bg-opacity-15 rounded-lg overflow-hidden w-full md:w-24">
+															<div className="pr-12">
+																Quantity:
+															</div>
+															<QuantityCounter
+																quantity={
+																	quantities[
+																		item._id
+																	] || 1
+																}
+																onIncrease={() =>
+																	increaseQuantity(
+																		item
+																	)
+																}
+																onDecrease={() =>
+																	decreaseQuantity(
+																		item
+																	)
+																}
+															/>
+														</div>
+													</div>
+
+													<div className="ml-0 md:ml-12 mb-2 w-full md:w-[100px] text-black">
+														Total:
+														<span className="pl-20 text-[18px] font-bold text-blue-600">
+															$
+															{(
+																Number(
+																	item.price.replace(
+																		/,/g,
+																		""
+																	)
+																) *
+																(quantities[
+																	item._id
+																] || 1)
+															).toLocaleString()}
+														</span>
+													</div>
+												</div>
+											</div>
+											<div
+												className="flex items-center justify-end  h-[30px] hover:bg-opacity-50 cursor-pointer"
+												title="Remove from cart"
+											>
+												{deletingItems[item._id] && (
+													<LoadingSpinner />
+												)}
+												{!deletingItems[item._id] && (
+													<MdDelete
+														className="text-red-500 text-xl scale-125"
+														onClick={() =>
+															handleDelete(
+																item._id
 															)
 														}
 													/>
-												</div>
-											</div>
-
-											<div className="ml-40 mb-2 w-[100px]">
-												<span className="text-[18px] font-bold text-blue-600">
-													$
-													{(
-														Number(
-															item.price.replace(
-																/,/g,
-																""
-															)
-														) *
-														(quantities[item._id] ||
-															1)
-													).toLocaleString()}
-												</span>
+												)}
 											</div>
 										</div>
-
-										<h2 className="text-2xl font-bold mb-2 text-black">
-											{item.brand}&nbsp;{item.car_model}
-										</h2>
-										<h3 className="line-clamp-2">
-											{item.bio}
-										</h3>
-
-										<div className="flex text-2xl text-yellow-600 cursor-pointer">
-											{"★".repeat(
-												Math.round(averageRating)
-											)}
-											{"☆".repeat(
-												5 - Math.round(averageRating)
-											)}
-										</div>
-									</div>
-									<div
-										className="flex h-[30px] hover:bg-opacity-50 cursor-pointer"
-										title="Remove from cart"
-									>
-										{deletingItems[item._id] && (
-											<LoadingSpinner />
-										)}
-										{!deletingItems[item._id] && (
-											<MdDelete
-												className="text-red-500 text-xl"
-												onClick={() =>
-													handleDelete(item._id)
-												}
-											/>
-										)}
-									</div>
-								</div>
-							</section>
-						);
-					})}
-				<div className="flex justify-end pt-10 text-white font-bold text-2xl">
+									</section>
+								);
+							})}
+						</div>
+					)}
+						<div className="flex justify-end pt-10 text-white font-bold text-2xl">
 					Total Price: ${calculateTotalPrice()}
 				</div>
-				<div className="flex justify-end py-10">
-					<Link to="/shop/payment">
-						<div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+				<div className="flex justify-end py-1">
+					<Link
+						to={{
+							pathname: "/shop/payment",
+							search: createSearchParams({
+								totalPrice: calculateTotalPrice().toString(),
+							}).toString(),
+						}}
+					>
+						<div 	className="detail-button bg-white text-black mt-12 px-4 py-2 md:px-6 md:py-3 w-full lg:h-[50px] justify-center flex hover:bg-black transition-all duration-300 ease-in-out hover:text-white  font-bold text-sm md:text-base rounded-3xl text-center
+						  before:ease relative h-12 overflow-hidden border-gray-600 border shadow-2xl  before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-12 before:bg-white before:opacity-50 before:duration-700 hover:shadow-gray-500 font-poppins hover:before:-translate-x-[290px] md:hover:before:-translate-x-[220px]
+						  ">
 							Proceed to Payment
 						</div>
 					</Link>
 				</div>
+				</div>
 			</div>
 		</section>
 	);
-}
+};
 
-export default UserCart
+export default UserCart;
--- client/src/_shop/payment/Payment.tsx
+++ client/src/_shop/payment/Payment.tsx
@@ -1,20 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { FaCcMastercard } from "react-icons/fa";
-import { FaCcVisa } from "react-icons/fa6";
+import { useLocation } from "react-router-dom";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
+import Cards from "react-credit-cards-2";
 
 const Payment = () => {
-
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const totalPrice = searchParams.get("totalPrice");
     const [countries, setCountries] = useState([]);
 
     useEffect(() => {
         const fetchCountries = async () => {
             try {
-                const response = await axios.get("https://restcountries.com/v3.1/all");
-                const countryOptions = response.data.map((country) => ({
-                    value: country.cca2,
-                    label: country.name.common,
-                })).sort((a, b) => a.label.localeCompare(b.label));
+                const response = await axios.get(
+                    "https://restcountries.com/v3.1/all"
+                );
+                const countryOptions = response.data
+                    .map((country) => ({
+                        value: country.cca2,
+                        label: country.name.common,
+                    }))
+                    .sort((a, b) => a.label.localeCompare(b.label));
                 setCountries(countryOptions);
             } catch (error) {
                 console.error("Error fetching countries:", error);
@@ -24,70 +31,103 @@ const Payment = () => {
         fetchCountries();
     }, []);
 
-	return (
-		<section className="text-white mt-44">
-			<div className="container mx-auto p-10 bg-white bg-opacity-30 backdrop-blur-md rounded-3xl">
-				<h2 className="text-2xl font-bold mb-4">Card Info</h2>
-				<div>
-					<div className="mb-4">
-						<label className="block text-sm font-bold mb-2">
-							Card Numbers
-						</label>
-						<div className="flex">
-                        <input 
-							type="text" 
-							placeholder="1234 1234 1234 1234" 
-							className="border border-white input input-bordered w-full max-w-xs" 
-						/>
-							<div className="flex items-center ml-2 space-x-3">
-								<FaCcVisa size={30} />
-								<FaCcMastercard size={30} />
-							</div>
-						</div>
-					</div>
-					<div className="mb-4">
-						<label className="block  text-sm font-bold mb-2">
-							Expired Day
-						</label>
-                        <input 
-							type="text" 
-							placeholder="MM/YY" 
-							className="border border-white input input-bordered w-full max-w-xs" 
-						/>
-					</div>
-					<div className="mb-4">
-						<label className="block  text-sm font-bold mb-2">
-							CVV
-						</label>
-                        <input 
-							type="text" 
-							placeholder="CVV" 
-							className="border border-white input input-bordered w-full max-w-xs" 
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							className="block text-sm font-bold mb-2" >
-							Country
-						</label>
-						<select className="select select-bordered w-full max-w-xs rounded border-white">
+    const [state, setState] = useState({
+        number: "",
+        name: "",
+        expiry: "",
+        cvc: "",
+        focus: "",
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setState((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleInputFocus = (e) => {
+        setState((prev) => ({ ...prev, focus: e.target.name }));
+    };
+
+    return (
+        <section className="text-black mt-10 md:mt-36">
+            <div className="container mx-auto p-6 md:p-10 bg-gray-800 bg-opacity-90 backdrop-blur-md rounded-3xl">
+                <h2 className="text-2xl md:text-3xl font-bold mb-6 text-white">Payment Information</h2>
+                <div className="flex justify-center mt-6 mb-12 scale-75 transform md:scale-100 md:hover:scale-110 xl:scale-[1.4] xl:hover:scale-[1.5] md:pb-12 md:pt-12 duration-300 ease-in-out">
+                    <Cards
+                        number={state.number}
+                        expiry={state.expiry}
+                        cvc={state.cvc}
+                        name={state.name}
+                        focused={state.focus}
+                    />
+                </div>
+                <div className="mt-4 grid grid-cols-1 gap-4 md:gap-6">
+                    <input
+                        type="text"
+                        name="number"
+                        className="form-control bg-gray-100 p-2 rounded-lg"
+                        placeholder="Card Number"
+                        value={state.number}
+                        maxLength={16}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="name"
+                        className="form-control bg-gray-100 p-2 rounded-lg"
+                        placeholder="Name on Card"
+                        value={state.name}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        required
+                    />
+                    <div className="grid grid-cols-2 gap-4 ">
+                        <input
+                            type="text"
+                            name="expiry"
+                            className="form-control bg-gray-100 p-2 rounded-lg"
+                            placeholder="Valid Thru (MM/YY)"
+                            pattern="\d\d/\d\d"
+                            value={state.expiry}
+                            maxLength={4}
+                            onChange={handleInputChange}
+                            onFocus={handleInputFocus}
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="cvc"
+                            className="form-control bg-gray-100 p-2 rounded-lg"
+                            placeholder="CVC"
+                            pattern="\d{3,4}"
+                            maxLength={3}
+                            value={state.cvc}
+                            onChange={handleInputChange}
+                            onFocus={handleInputFocus}
+                            required
+                        />
+                        <select className="select col-span-2 bg-white select-bordered w-full rounded-lg border-gray-300">
                             {countries.map((country) => (
                                 <option key={country.value} value={country.value}>
                                     {country.label}
                                 </option>
                             ))}
-						</select>
-					</div>
-					<button
-						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-						type="submit"
-					>
-						Payment
-					</button>
-				</div>
-			</div>
-		</section>
-	);
+                        </select>
+                    </div>
+                    <button
+                      	className="detail-button bg-white text-black mt-12 px-4 py-2 md:px-6 md:py-3 w-full lg:h-[50px] justify-center flex hover:bg-black transition-all duration-300 ease-in-out hover:text-white  font-bold text-sm md:text-base rounded-3xl text-center
+						  before:ease relative h-12 overflow-hidden border-gray-600 border shadow-2xl  before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-12 before:bg-white before:opacity-50 before:duration-700 hover:shadow-gray-500 font-poppins hover:before:-translate-x-[290px] md:hover:before:-translate-x-[720px]
+						  "
+                        type="submit"
+                    >
+                        Payment
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Payment;
