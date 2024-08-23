899b7ff11635380e43ffd690231f3670a9b4f9e7
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-08-24 04:33:35 +0700

    thêm địa chỉ vô payment

--- client/src/_shop/PaymentLayout.tsx
+++ client/src/_shop/PaymentLayout.tsx
@@ -3,26 +3,25 @@ import Navbar from "./common/Navbar";
 import OverviewCart from "./payment/OverviewCart";
 import Payment from "./payment/Payment";
 
-
 const PaymentLayout = () => {
 	return (
-		<section className="w-full h-[1900px] md:h-[1700px] xl:h-[1050px] bg-primary">
+		<section className="w-full h-[1900px] md:h-[1700px] xl:h-[1100px] bg-primary">
 			<div className="w-full ">
 				<div className="items-start justify-center">
 					<div className="w-full bg-primary">
 						<Navbar />
 					</div>
 				</div>
-				
-					<div className="md:grid p-5 pt-1 md:grid-cols-2 md:px-12 xl:px-[100px] md:gap-10">
-						<div className=" bg-primary">
-							<Payment />
-						</div>
-						<div className=" bg-primary ">
-							<OverviewCart />
-						</div>
+
+				<div className="md:grid p-5 pt-1 md:grid-cols-2 md:px-12 xl:px-[100px] md:gap-10">
+					<div className=" bg-primary">
+						<Payment />
 					</div>
-					<div className="items-start justify-center">
+					<div className=" bg-primary ">
+						<OverviewCart />
+					</div>
+				</div>
+				<div className="items-start justify-center">
 					<div className="w-full bg-primary pt-24">
 						<Footer />
 					</div>
