0555cb1aff63d824a2b7da53d7ce14bbabaa7ecd
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-09-05 15:21:02 +0700

    delete quantity and check

--- client/src/_shop/cart/UserCart.tsx
+++ client/src/_shop/cart/UserCart.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from "@/components/social/ui/common/LoadingSpinner";
 import calculateAvgRating from "@/utils/calculateAvgRating";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { MdCheck, MdDelete } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 import QuantityCounter from "@/utils/QuantityCounter";
 import { toast, Toaster } from "react-hot-toast";
 import { Link, createSearchParams } from "react-router-dom";
@@ -162,7 +162,7 @@ const UserCart = () => {
 									<section className="mb-4" key={item._id}>
 										<div
 											data-aos="fade-left"
-											className="flex flex-col md:flex-row bg-gradient-to-r from-white to-gray-400 shadow-2xl shadow-black p-3 mb-7 rounded-2xl h-full hover:bg-gradient-to-r hover:from-gray-100 hover:to-white duration-500 transition-all ease-in-out"
+											className="flex flex-col md:flex-row bg-gradient-to-r from-white to-gray-400 shadow-2xl shadow-black p-3 md:p-5 mb-7 rounded-2xl h-full hover:bg-gradient-to-r hover:from-gray-100 hover:to-white duration-500 transition-all ease-in-out"
 										>
 											<div className="relative  flex items-center">
 												<Link
@@ -246,8 +246,8 @@ const UserCart = () => {
 														</span>
 													</div>
 												</div>
-												<div className="md:block hidden">
-													<div className="xl:mt-14 md:mt-52 items-end w-full justify-end  flex text-black gap-7">
+												{/* <div className="md:block hidden">
+													<div className="xl:mt-14 md:mt-52 items-end w-full justify-end p-5 flex text-black gap-7">
 														<div className="flex items-center hover:bg-gray-100 hover:bg-opacity-15 rounded-lg overflow-hidden w-full md:w-24 top-[7px] scale-110 relative">
 															<QuantityCounter
 																quantity={
@@ -267,23 +267,8 @@ const UserCart = () => {
 																}
 															/>
 														</div>
-														<div
-															className="w-[36px] h-[36px] cursor-pointer rounded-full bg-white shadow-md shadow-black flex items-center justify-center   text-blackhover:bg-white transition-all duration-300 ease-in-out green-400  font-bold text-md md:text-basetext-center
-						 									 before:ease relative overflow-hidden border-gray-600 border  before:absolute before:right-0 before:top-0 before:h-12 before:w-4 before:translate-x-12 before:rotate-12 before:bg-green-500 before:opacity-50 before:duration-700 hover:shadow-green-500 font-poppins hover:before:-translate-x-[290px] md:hover:before:-translate-x-[39px]"
-															onClick={() =>
-																handleClick(
-																	item._id
-																)
-															}
-														>
-															{checkedItems[
-																item._id
-															] && (
-																<MdCheck className="text-green-500 rounded-full w-full h-full text-4xl font-bold" />
-															)}
-														</div>
 													</div>
-												</div>
+												</div> */}
 											</div>
 											<div className="xl:hidden block">
 												<div className="w-full justify-center flex">
@@ -293,7 +278,7 @@ const UserCart = () => {
 
 											<div className="block md:hidden">
 												<div className="ml-3 mt-4 md:ml-12 ">
-													<div className="ml-0 md:ml-7 mb-2 text-black">
+													{/* <div className="ml-0 md:ml-7 mb-2 text-black">
 														<div className="flex items-center hover:bg-gray-100 hover:bg-opacity-15 rounded-lg overflow-hidden w-full md:w-24">
 															<div className="pr-12">
 																Quantity:
@@ -316,7 +301,7 @@ const UserCart = () => {
 																}
 															/>
 														</div>
-													</div>
+													</div> */}
 
 													<div className="ml-0 md:ml-12 mb-2 w-full md:w-[100px] text-black">
 														Total:
@@ -360,22 +345,6 @@ const UserCart = () => {
 																	/>
 																)}
 															</div>
-													<div
-														className="w-[40px] h-[40px] cursor-pointer rounded-full bg-white shadow-md shadow-black text-blackhover:bg-white transition-all duration-300 ease-in-out green-400  font-bold text-md md:text-basetext-center
-						  before:ease relative overflow-hidden border-gray-600 border  before:absolute before:right-0 before:top-0 before:h-12 before:w-4 before:translate-x-12 before:rotate-12 before:bg-green-500 before:opacity-50 before:duration-700 hover:shadow-green-500 font-poppins hover:before:-translate-x-[290px] md:hover:before:-translate-x-[39px]"
-														onClick={() =>
-															handleClick(
-																item._id
-															)
-														}
-													>
-														{checkedItems[
-															item._id
-														] && (
-															<MdCheck className="text-green-500 rounded-full w-full h-full text-4xl font-bold" />
-														)}
-													</div>
-													
 												</div>
 											</div>
 										</div>
